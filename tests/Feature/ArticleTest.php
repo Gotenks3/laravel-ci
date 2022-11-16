67456044ce47cd9af585dd97683984a6741c398a
Gotenks3 <riku.murakami333@gmail.com>
2022-11-16 18:49:39 +0900

    調整

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -7,6 +7,7 @@ use App\Article;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\User;
 use Tests\TestCase;
 
 class ArticleTest extends TestCase
@@ -18,6 +19,29 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
 
         $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
 
         $this->assertFalse($result);
     }
